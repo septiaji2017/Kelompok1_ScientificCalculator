7fda34fcff8db4f15049b25e92235013dcdc6cac
septiaji2017 <septiaji2017@gmail.com>
2023-02-06 09:27:41 +0700

    Upload file berekstensi H dengan nama Ridha.h

---
+++ Ridha.h
@@ -0,0 +1,17 @@
+#ifndef Ridha_h
+#define Ridha_h
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int f_pangkat(int x, int y){
+	x=pow(x,y);
+	return x;
+}
+
+int f_akar(int x){
+	x=sqrt(x);
+	return x;
+}
+
+#endif
