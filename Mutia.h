5c1ac4030a498a9d846d61aad761651e034b0738
Mutia Hardita <tia.ditaa@gmail.com>
2023-02-06 11:05:36 +0700

    Update Mutia.h
    revisi 2

--- Mutia.h
+++ Mutia.h
@@ -5,9 +5,6 @@
  
 int faktorial(int input){
 	int i, hasil = 1;
-	printf("Masukkan angka : ");
-	scanf("%d", &input);
-	printf("\n %d! = ", input);
 	for(i=input; i>=1; i--){
 		printf(" %d ", i);
 		if((i <= input) && (i!=1)){		
