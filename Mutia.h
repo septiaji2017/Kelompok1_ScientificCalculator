44ccb07c8147942bdc680116da563fa375372e7e
Mutia Hardita <tia.ditaa@gmail.com>
2023-02-06 10:30:42 +0700

    Update Mutia.h
    Revisi 1

--- Mutia.h
+++ Mutia.h
@@ -1,9 +1,23 @@
 #ifndef Mutia_h
 #define Mutia_h
+#include <stdio.h>
+#include <stdlib.h>
  
-std::string cetak(){ //sebuah function di file header hallo.h
-
-    return ;
+int faktorial(int input){
+	int i, hasil = 1;
+	printf("Masukkan angka : ");
+	scanf("%d", &input);
+	printf("\n %d! = ", input);
+	for(i=input; i>=1; i--){
+		printf(" %d ", i);
+		if((i <= input) && (i!=1)){		
+			printf ("x");
+		}
+		hasil *= i;
+	}
+	
+	printf(" = %d", hasil);
+	return (hasil);
 }
  
 #endif
