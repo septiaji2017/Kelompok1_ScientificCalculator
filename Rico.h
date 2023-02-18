2e29f1fda2b40e5bb921ba89bb374def91ede10d
Stefanus Rico Pandapotan Situngkir <stefanus.rico70@gmail.com>
2023-02-18 16:26:28 +0700

    memperbarui integral, dan menambahkan limit dan persamaan kuadrat

--- Rico.h
+++ Rico.h
@@ -22,6 +22,24 @@ float inversTan(float x){
 	return value;
 }
 
+float inversSec(float x){
+	double value;
+	value = round(acos(1/x)*180/pi);
+	return value;	
+}
+
+float inversCotan(float x){
+	double value;
+	value = round(atan(1/x)*180/pi);	
+	return value;
+}
+
+float inversCosec(float x){
+	double value;
+	value = round(asin(1/x)*180/pi);		
+	return value;
+}
+
 float logaritmaNatural(float ln){
 	float hasil;
 	hasil = log(ln);
@@ -35,24 +53,49 @@ float logaritma(float log){
 }
 
 float fungsi(float x) {
-  return x*x + 3*x - 5;
+  return 3*x*x + 2*x + 1;
 }
 
 float trapezoid(float bawah, float atas, int n, float (*fungsi)(float)) {
+  int i;
   float ax = (atas - bawah) / n;
   float sum = 0.5 * (fungsi(bawah) + fungsi(atas));
-  for (int i = 1; i < n; i++) {
+  for (i = 1; i < n; i++) {
     sum += fungsi(bawah + i * ax);
   }
   return (ax * sum);
 }
 
 float polinomial(float coef[11], int derajat, float x){
+	int i;
 	float y = 0.0;
-	for(int i = 0; i <= derajat; i++){
+	for(i = 0; i <= derajat; i++){
 		y += coef[i] * pow(x, i); 
 	}
 	return y;
+}
+
+float operasiFaktorPositif(float coef[11], float akarPositif, float denom, float nilaiPositif){
+	akarPositif = (-coef[1] + sqrt(coef[1]*coef[1]- 4*coef[2]*coef[0]));
+	denom = 2*coef[2];
+	nilaiPositif = akarPositif/denom;
+	return nilaiPositif;
+}
+
+float operasiFaktorNegatif(float coef[11], float akarNegatif, float denom, float nilaiNegatif){
+	akarNegatif = (-coef[1] - sqrt(coef[1]*coef[1] - 4*coef[2]*coef[0]));
+	denom = 2*coef[2]; 
+	nilaiNegatif = akarNegatif/denom;
+	return nilaiNegatif;
+}
+
+double limit(float coef[11], int derajat, double inf){
+	int i;
+	double l = 0.0;
+	for(i = 1; i <= derajat; i++){
+			l = coef[i] * pow(inf, i);
+	}
+	return l;
 }
 
 
