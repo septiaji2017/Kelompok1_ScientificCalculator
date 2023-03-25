1a16c271ad96135981ea8567ef1b3565ff57f80b
Naufal Syafiq Somantri <naufal22335@gmail.com>
2023-03-25 13:44:58 +0700

    Up C

---
+++ Naufal.h
@@ -0,0 +1,25 @@
+#ifndef Naufal_h
+#define Naufal_h
+#include <stdio.h>
+
+float CtoK(float Kelvin);
+
+float FtoK(float Kelvin);
+
+float KtoC(float Celsius);
+
+float FtoC(float Celsius);
+
+float KtoF(float Fahrenheit);
+
+float CtoF(float Fahrenheit);
+
+float pertambahan(float x, float y);
+
+float pengurangan(float x, float y);
+
+float perkalian(float a, float b);
+
+float pembagian(float dividend, float divisor);
+
+#endif
