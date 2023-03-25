7e5511323c2a01db8e5f6c66838f41041713b50b
tiaditaa <118504168+tiaditaa@users.noreply.github.com>
2023-03-25 09:30:05 +0700

    Create Mutia.h
    membuat mutia.h

---
+++ Mutia.h
@@ -0,0 +1,23 @@
+#ifndef Mutia_h
+#define Mutia_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int factorial(int n);
+
+//program trigonometri
+//mencari nilai dari derajat trigonometri
+double sine(double x);
+
+// Fungsi untuk menghitung nilai kosinus menggunakan deret Maclaurin
+double cosine(double x);
+
+double tangent(double x);
+
+double cosecant(double x);
+
+double secant(double x);
+
+double cotan(double x);
+#endif
