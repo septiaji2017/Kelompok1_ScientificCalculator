59743f6776c8a419de1a1c90bc3840619f4999f4
Stefanus Rico Pandapotan Situngkir <stefanus.rico70@gmail.com>
2023-03-25 13:47:42 +0700

    no message

---
+++ Rico.h
@@ -0,0 +1,27 @@
+#ifndef rico_h
+#define rico_h
+#include <stdio.h>
+#define INFINITY (1.0/0.0)
+#define UNDEFINED (0.0/0.0)
+
+double round_num(double num);
+
+double arcsin(double x);
+
+double arccos(double x);
+
+double arctan(double x);
+
+double arcsec(double x);
+
+double arccsc(double x);
+
+double arccot(double x);
+
+double invsTrigonometri(char *invsTrigo);
+
+double log_2(double x); 
+
+double log_10(double x);
+
+#endif
