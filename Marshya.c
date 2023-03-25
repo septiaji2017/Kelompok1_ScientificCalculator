ff2f2b6c69328e00ae20c27f538ef543372b3f34
septiaji2017 <septiaji2017@gmail.com>
2023-03-25 20:13:01 +0700

    menambahkan folder pi.h

---
+++ Marshya.c
@@ -0,0 +1,30 @@
+#include "Marshya.h"
+
+float flr(float x)
+{
+	float result;
+	result = floor(x);
+	return result;
+}
+
+float cl(float x)
+{
+	float result;	
+	result = ceil(x);
+	return result;
+}
+
+float rnd(float x)
+{
+	float result;	
+	result = round(x);	
+	return result;
+}
+
+int absolute(int y)
+{
+	int result;	
+	result = abs(y);	
+	return result;
+}
+
---
+++ Marshya.h
@@ -0,0 +1,11 @@
+#ifndef Marshya_h
+#define Marshya_h
+#include <stdio.h>
+#include <math.h>
+
+float flr(float x);
+float cl(float x);
+float rnd(float x);
+int absolute(int x);
+
+#endif
