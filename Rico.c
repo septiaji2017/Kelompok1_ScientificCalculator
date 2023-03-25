c84f007df1fcffba98cb74749b76bd898aa4ef66
Stefanus Rico Pandapotan Situngkir <stefanus.rico70@gmail.com>
2023-03-25 13:44:20 +0700

    no message

---
+++ Rico.c
@@ -0,0 +1,149 @@
+#include "Rico.h"
+#include "Ridha.h"
+#include "pi.h"
+
+double round_num(double num) 
+{
+    int sign = (num >= 0) ? 1 : -1;
+    double abs_num = num * sign;
+    int int_part = (int) abs_num;
+    double frac_part = abs_num - int_part;
+
+    if (frac_part >= 0.5) {
+        return sign * (int_part + 1);
+    } else {
+        return sign * int_part;
+    }
+}
+
+double arcsin(double x) 
+{
+    if (x < -1.0 || x > 1.0) {
+        return UNDEFINED;
+    }
+    if (x == -1.0) {
+        return -pi/2.0;
+    }
+    if (x == 1.0) {
+        return pi/2.0;
+    }
+    double sum = x;
+    double term = x;
+    int n = 1;
+    while (n < 1000 && term != 0.0) {
+        term *= x*x*(2.0*n-1)/(2.0*n*(2.0*n+1));
+        sum += term;
+        n++;
+    }
+    return sum;
+}
+
+double arccos(double x) 
+{
+    if (x < -1.0 || x > 1.0) {
+        return UNDEFINED;
+    }
+    if (x == -1.0) {
+        return pi;
+    }
+    if (x == 1.0) {
+        return 0.0;
+    }
+    return pi/2.0 - arcsin(x);
+}
+
+double arctan(double x) 
+{
+    if (x == 0.0) {
+        return 0.0;
+    }
+    if (x > 1.0) {
+        return pi/2.0 - arctan(1.0/x);
+    }
+    if (x < -1.0) {
+        return -pi/2.0 - arctan(1.0/x);
+    }
+    double sum = x;
+    double term = x;
+    int n = 1;
+    while (n < 1000 && term != 0.0) {
+        term *= -x*x*(2.0*n-1)/(2.0*n+1);
+        sum += term;
+        n++;
+    }
+    return sum;
+}
+
+double arcsec(double x)
+{
+	if(x == 0){
+		return UNDEFINED;
+	}
+	return arccos(1.0/x);
+}
+
+double arccsc(double x)
+{
+	if(x == 0){
+		return UNDEFINED;
+	}
+	return arcsin(1.0/x);
+}
+
+double arccot(double x)
+{
+	return arctan(1.0/x);
+}
+
+double invsTrigonometri(char *invsTrigo)
+{
+	double x;
+	if (strstr(invsTrigo, "arcsin")){
+		sscanf(invsTrigo,"arcsin%lf",&x);
+		return arcsin(x);
+	}
+	if (strstr(invsTrigo, "arccos")){
+		sscanf(invsTrigo,"arccos%lf",&x);
+		return arccos(x);
+	}
+	if (strstr(invsTrigo, "arctan")){
+		sscanf(invsTrigo,"arctan%lf",&x);
+		return arctan(x);
+	}
+	if (strstr(invsTrigo, "arccsc")){
+		sscanf(invsTrigo,"arccsc%lf",&x);
+		return arccsc(x);
+	}
+	if (strstr(invsTrigo, "arcsec")){
+		sscanf(invsTrigo,"arcsec%lf",&x);
+		return arccsc(x);
+	}
+	if (strstr(invsTrigo, "arccot")){
+		sscanf(invsTrigo,"arccot%lf",&x);
+		return arccot(x);
+	}
+}
+
+double log_2(double x) 
+{
+    double result = 0.0;
+    if(x < 0){
+    	return 0;
+	}
+    while (x > 1) {
+        result++;
+        x /= 2.0;
+    }
+    while (x < 0.5) {
+        result--;
+        x *= 2.0;
+    }
+    return result;
+}
+
+double log_10(double x) 
+{
+    double result = log_2(x) / log_2(10);
+    return result;
+}
+
