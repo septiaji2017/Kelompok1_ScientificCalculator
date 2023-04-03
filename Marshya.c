d8af17f54644e9f67bf6d69f9353d741a432462f
MarsNrz <marshya.nurz@gmail.com>
2023-04-03 09:36:18 +0700

    mars tambahan clean code

--- Marshya.c
+++ Marshya.c
@@ -1,30 +1,34 @@
 #include "Marshya.h"
 
+//Program konversi nilai ke floor
 float flr(float x)
 {
-	float result;
-	result = floor(x);
-	return result;
+	float result;			//deklarasi variabel untuk menampung hasil
+	result = floor(x);		//
+	return result;			//
 }
 
+//Program konversi nilai ke ceil
 float cl(float x)
 {
-	float result;	
-	result = ceil(x);
-	return result;
+	float result;			//deklarasi variabel untuk menampung hasil
+	result = ceil(x);		//
+	return result;			//
 }
 
+//Program konversi nilai ke round
 float rnd(float x)
 {
-	float result;	
-	result = round(x);	
-	return result;
+	float result;			//deklarasi variabel untuk menampung hasil
+	result = round(x);		//
+	return result;			//
 }
 
+//Program konversi nilai ke absolute
 int absolute(int y)
 {
-	int result;	
-	result = abs(y);	
-	return result;
+	int result;				//deklarasi variabel untuk menampung hasil
+	result = abs(y);		//
+	return result;			//
 }
 
